af01c6932b2022084e68a9a5fb900a786c3d4efc
Averydreaming <51030996+Averydreaming@users.noreply.github.com>
2023-01-11 21:14:04 +0800

    Add files via upload

--- src/ICache.v
+++ src/ICache.v
@@ -1,31 +1,23 @@
-`ifndef ICACHE
-`define ICACHE
-`include "op_map.v"
+`include "defines.v"
 
-//first finish
-//相当于 instruction queue 我的理解是为了解决循环问题
-//每一个周期从内存拿一条指令
-//每一个周期将一条指令拿去处理
 module ICache (
     input  wire              clk,
     input  wire              rst,
     input  wire              rdy,
     //每一个周期从内存拿一条指令 from Memctrl
-    output reg             ICache_need_update_instr,//可以输入一条指令
-    output reg [31: 0]     instr_address,
-    input  wire             instr_valid, //存在输入
-    input  wire [31: 0]     instr,
+    output reg               ICache_need_update_instr,//可以输入一条指令
+    output reg [31:0]        instr_address,
+    input  wire              instr_valid, //存在输入
+    input  wire [31:0]       instr,
 
     //每一个周期将一条IF pc需要的指令拿去InstructionFetch处理
-    input  wire [31: 0]     pc,//从InstructionFetch获取的next周期需要的instr的pc
+    input  wire [31:0]      pc,//从InstructionFetch获取的next周期需要的instr的pc
     output reg              instr_IF_valid, 
-    output reg  [31: 0]     instr_IF,
+    output reg  [31:0]      instr_IF,
 
     input  wire             jump_wrong
 
 );
-
-  
     reg  [31: 0]    cache_IQ       [511 : 0];
     reg  [511:0]    used_IQ;
     reg  [17: 11]    tag           [511 :0];//直接 cache[PC] 空间不够
@@ -60,5 +52,5 @@ module ICache (
         end
     end
 
+
 endmodule
-`endif
